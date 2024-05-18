17e6da444af372e571968bf5d7b8eb3fd7cc4458
Akash Shaw <shawakash2003@gmail.com>
2024-05-18 17:10:08 +0530

    chore: adds consumer class to consume the data

---
+++ backend/worker/src/kafka/consumer.js
@@ -0,0 +1,60 @@
+const { kafka } = require("./admin");
+const { sendEmail } = require("../utils/utils");
+
+class ConsumerWorker {
+  static instance;
+  consumer;
+
+  constructor() {}
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new ConsumerWorker();
+    }
+    return this.instance;
+  }
+
+  async connectConsumer(groupId, topics, fromBeginning) {
+    this.consumer = kafka.consumer({ groupId });
+    await this.consumer.connect();
+    console.log(`consumer connected successfully`);
+    await this.consumer.subscribe({ topics, fromBeginning });
+    return this.consumer;
+  }
+
+  async disconnectConsumer() {
+    await this.consumer.disconnect();
+    return;
+  }
+
+  async runConsumer() {
+    await this.consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        try {
+          const payload = JSON.parse(message.value.toString() || "");
+
+          switch (topic) {
+            case "mail":
+              await sendEmail(
+                payload.listId,
+                payload.emailBody,
+                payload.subject,
+              );
+              break;
+
+            default:
+              console.log("That topic is not yet supported");
+              break;
+          }
+        } catch (e) {
+          console.log(e);
+          return;
+        }
+      },
+    });
+  }
+}
+
+module.exports = {
+  ConsumerWorker,
+};
